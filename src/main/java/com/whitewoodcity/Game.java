523daa233a43e451860ed3c7af569bfc396ee164
chengen.zhao@mail.mcgill.ca <chengen.zhao@mail.mcgill.ca>
2021-05-25 21:17:35 +0800

    use default cursor image

--- src/main/java/com/whitewoodcity/Game.java
+++ src/main/java/com/whitewoodcity/Game.java
@@ -1,11 +1,13 @@
 package com.whitewoodcity;
 
+import com.almasb.fxgl.app.CursorInfo;
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
+import javafx.scene.Cursor;
 
 public class Game extends GameApplication {
     @Override
     protected void initSettings(GameSettings settings) {
-
+        settings.setDefaultCursor(new CursorInfo("cursor.png",3,3));
     }
 }
